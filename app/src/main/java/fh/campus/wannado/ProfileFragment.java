8d9bd2abcd40180d3f49933e2be8915cdc8ced27
Jakub Róg <jakubrog25@gmail.com>
2021-01-06 11:18:50 +0100

    added lombok dependency, users collection classes, loading circle in fragment profile, changed names of constraints + refactored

--- app/src/main/java/fh/campus/wannado/ProfileFragment.java
+++ app/src/main/java/fh/campus/wannado/ProfileFragment.java
@@ -6,78 +6,62 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.ListenerRegistration;
 
-import java.util.concurrent.Executor;
+import fh.campus.wannado.collections.users.UsersDocument;
+import fh.campus.wannado.collections.users.UsersCollection;
+import fh.campus.wannado.databinding.FragmentProfileBinding;
 
 public class ProfileFragment extends Fragment {
-    TextView username, email;
-    FirebaseAuth mFirebaseAuth;
-    FirebaseFirestore firestore;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    String userId;
-    Button btnLogout;
 
+    private FragmentProfileBinding binding;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        username = view.findViewById(R.id.textUsername);
-        email = view.findViewById(R.id.textEmail);
-        btnLogout = view.findViewById(R.id.buttonLogout);
-
-        firestore = FirebaseFirestore.getInstance();
-        mFirebaseAuth = FirebaseAuth.getInstance();
-
-        userId = mFirebaseAuth.getCurrentUser().getUid();
-
-        DocumentReference documentReference = firestore.collection("users").document(userId);
-
-        documentReference.addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                username.setText(value.getString("username"));
-                email.setText(value.getString("email"));
-            }
-        });
-
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
-            }
-        });
-
+        binding = FragmentProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        setUserInfo();
+        setButtonsListeners();
         return view;
     }
 
-/*
+    private void setButtonsListeners(){
+        binding.buttonLogout.setOnClickListener(v -> {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
 
-
-
-
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-
-
+    private void setUserInfo() {
+        showLoadingCircle();
+        UsersCollection.getCurrentUser(task -> {
+            if(task.isSuccessful()){
+                DocumentSnapshot document = task.getResult();
+                if(document.exists()){
+                    UsersDocument usersDocument = UsersCollection.getUserDocumentOf(document);
+                    binding.textEmail.setText(usersDocument.getEmail());
+                    binding.textUsername.setText(usersDocument.getUsername());
+                    binding.loadingPanel.setVisibility(View.GONE);
+                }
             }
-        });*/
+            hideLoadingCircle();
+        });
+    }
+
+    private void showLoadingCircle(){
+        binding.loadingPanel.setVisibility(View.VISIBLE);
+    }
+
+    private void hideLoadingCircle(){
+        binding.loadingPanel.setVisibility(View.GONE);
+    }
 }
+
---
+++ app/src/main/java/fh/campus/wannado/collections/users/UsersCollection.java
@@ -0,0 +1,32 @@
+package fh.campus.wannado.collections.users;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class UsersCollection {
+    private final static String COLLECTION_NAME = "users";
+    public final static String EMAIL_FIELD = "email";
+    public final static String USERNAME_FIELD = "username";
+
+    public static void getCurrentUser(OnCompleteListener<DocumentSnapshot> onCompleteListener){
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        getById(userId, onCompleteListener);
+    }
+
+    public static void getById(String userId, OnCompleteListener<DocumentSnapshot> onCompleteListener){
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = firestore.collection(COLLECTION_NAME).document(userId);
+        documentReference.get().addOnCompleteListener(onCompleteListener);
+    }
+
+    public static UsersDocument getUserDocumentOf(DocumentSnapshot documentSnapshot){
+        return UsersDocument.builder()
+                .email(documentSnapshot.getString(EMAIL_FIELD))
+                .username(documentSnapshot.getString(USERNAME_FIELD))
+                .build();
+    }
+
+}
