7f96a89a4d0b8c5fc5015b1e2add2349a35e4dd5
Daniel Kaminski <ninjadaniel@hotmail.com>
2021-01-10 11:39:01 +0100

    get Current Message from db

--- app/src/main/java/fh/campus/wannado/collections/messages/MessagesCollection.java
+++ app/src/main/java/fh/campus/wannado/collections/messages/MessagesCollection.java
@@ -1,5 +1,26 @@
 package fh.campus.wannado.collections.messages;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class MessagesCollection {
 
+    private final static String COLLECTION_NAME = "messages";
+    public final static String CHAT_FIELD = "chatID";
+    public final static String MESSAGES_FIELD = "messages";
+
+    public final static String SENDER_FIELD = "senderID";
+    public final static String TIMESTAMP_FIELD = "timestamp";
+    public final static String CONTENT_FIELD = "content";
+
+    public void getCurrentMessage(OnCompleteListener<QuerySnapshot> onCompleteListener){
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        String userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        firestore.collection(COLLECTION_NAME)
+                .whereArrayContains("senderID", userID)
+                .get()
+                .addOnCompleteListener(onCompleteListener);
+    }
 }
