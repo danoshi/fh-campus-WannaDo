e5daeb164e98dbc9ab53e106f53901b45982a093
Daniel Kaminski <ninjadaniel@hotmail.com>
2021-01-10 16:27:37 +0100

    fix for reading out of db in chat activity

--- app/src/main/java/fh/campus/wannado/activities/ChatDetailsActivity.java
+++ app/src/main/java/fh/campus/wannado/activities/ChatDetailsActivity.java
@@ -1,11 +1,16 @@
 package fh.campus.wannado.activities;
 
 import android.os.Bundle;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 import fh.campus.wannado.R;
 import fh.campus.wannado.collections.chats.ChatCollection;
@@ -13,19 +18,27 @@ import fh.campus.wannado.collections.chats.ChatDocument;
 
 public class ChatDetailsActivity  extends AppCompatActivity {
 
-    TextView textViewMessage;
+    TextView textViewMessage, textViewDate, textViewSender;
+    ArrayList<ChatDocument> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatdetails);
 
+        items = new ArrayList<>();
         textViewMessage = findViewById(R.id.textViewMessageDB);
+        textViewDate = findViewById(R.id.textViewDate);
+        textViewSender = findViewById(R.id.textViewSender);
         ChatCollection.getUserChats(task -> {
             if(task.isSuccessful()){
-                QuerySnapshot queryDocumentSnapshot = task.getResult();
-                ChatDocument chatDocument = queryDocumentSnapshot.toObjects(ChatDocument.class);
-                textViewMessage.setText(chatDocument.getLastMessage());
+                for(QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
+                    ChatDocument chatDocument = queryDocumentSnapshot.toObject(ChatDocument.class);
+                    items.add(chatDocument);
+                    textViewMessage.setText(chatDocument.getLastMessage());
+                    //textViewDate.setText(chatDocument.getLastMessageTime());
+                    textViewSender.setText(chatDocument.getParticipantsID().get(0));
+                }
             }
         });
     }
